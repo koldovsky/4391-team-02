49670eb328f54129538cf4757758eea7bf325ce1
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-08-15 19:56:51 +0200

    Complete task Messi's goals from CodeWars

--- js-tasks/messi-goals.js
+++ js-tasks/messi-goals.js
@@ -1,1 +1,6 @@
 //https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+// Oleksandr Romaniuk
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
