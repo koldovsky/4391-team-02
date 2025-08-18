2167a4dab4a595e6cd7d4a595a8ad733cf255d35
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-08-18 11:05:08 +0200

    Add swiperJS for carousel implementation

---
+++ js/services.js
@@ -0,0 +1,29 @@
+new Swiper('.services__cards', {
+  loop: true,
+  spaceBetween: 30,
+
+  // Pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+  breakpoints: { 
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
+});
