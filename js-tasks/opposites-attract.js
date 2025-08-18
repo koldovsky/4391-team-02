043c9aea2b201f19f80efa6089036745cc0da538
sesyacodez <62096135+sesyacodez@users.noreply.github.com>
2025-08-18 14:17:17 +0300

    Update opposites-attract.js

--- js-tasks/opposites-attract.js
+++ js-tasks/opposites-attract.js
@@ -4,3 +4,12 @@
 function lovefunc(flower1, flower2){
   return (flower1 + flower2) % 2 === 1;
 }
+
+//Stefania
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 !== flower2 % 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
