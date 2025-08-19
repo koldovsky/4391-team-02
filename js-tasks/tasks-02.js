af382e8dfcecca0f14cbea672d4d8abda7aa1b65
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-08-19 17:31:53 +0200

    Add solution for 'Find Maximum and Minimum Values of a List'

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,9 +1,26 @@
 // https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 function stringToArray(string) {
-    return string.split(' ');
+  return string.split(" ");
 }
 
 // https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
-    return dna.replace(/T/g, "U");
+  return dna.replace(/T/g, "U");
 }
+
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+var min = function(list) {
+  list.sort(function(a, b) {
+    return a - b;
+  });
+  return list[0];
+};
+
+var max = function(list) {
+  list.sort(function(a, b) {
+    return a - b;
+  });
+  return list[list.length - 1];
+};
+
+
