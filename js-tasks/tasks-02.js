5158407464c88a9785a02d142dfbe3ab1df16184
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-08-19 18:06:16 +0200

    Add solution for 'Beginner Series #2 Clock'

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -56,3 +56,8 @@ function getRealFloor(n) {
   }
   return n;
 }
+
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+function past(h, m, s) {
+  return 1000 * (h * 3600 + m * 60 + s);
+}
