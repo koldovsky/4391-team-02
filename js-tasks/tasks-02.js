fe95689575a835ab38dbab1dd5b52f2b2a5cdca3
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-08-19 18:02:42 +0200

    Add solution for 'What's the real floor?'

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -48,3 +48,11 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 function nthEven(n) {
   return n * 2 - 2;
 }
+
+// https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+function getRealFloor(n) {
+  if (n > 0) {
+    return n < 13 ? n - 1 : n - 2;
+  }
+  return n;
+}
