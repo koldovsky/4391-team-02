ad2d8dacaa66f61cf65a27975fbff869f525d3b2
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-08-19 17:54:14 +0200

    Add solution for 'Get Nth Even Number'

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -43,3 +43,8 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 
   return years < 0 ? -years : years;
 }
+
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+function nthEven(n) {
+  return n * 2 - 2;
+}
