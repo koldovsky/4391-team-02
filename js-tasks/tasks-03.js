6ca187ea02932b0c315acecba189154cfd98e5f7
Dmitrij <124076559+internetserfer@users.noreply.github.com>
2025-08-21 19:12:32 +0000

    Task 3 OOP

---
+++ js-tasks/tasks-03.js
@@ -0,0 +1,21 @@
+/* https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+   Povstianyi D */
+function circleCircumference(circle) {
+    return 2 * circle.radius * Math.PI;
+}
+
+/* https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+   Povstianyi D */
+function giveMeFive(obj) {
+    const array = [];
+    for (var key in obj) {
+        if (key.length === 5) {
+            array.push(key);
+        }
+        if (obj[key].length === 5) {
+            array.push(obj[key]);
+        }
+    }
+    return array;
+}
+
