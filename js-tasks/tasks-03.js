3c795584cda78aa8a09b404379c9e5788ae09660
Mishail Vasiluev Kn-222b <167981278+Mayschy@users.noreply.github.com>
2025-08-22 07:04:46 +0000

    add checks on 2 task js, also add 3-4 task

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,20 +1,68 @@
 /* https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
    Povstianyi D */
+
 function circleCircumference(circle) {
-    return 2 * circle.radius * Math.PI;
+  return 2 * circle.radius * Math.PI;
 }
 
 /* https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
    Povstianyi D */
+//    Vasyliev add more checks
+
 function giveMeFive(obj) {
-    const array = [];
-    for (var key in obj) {
-        if (key.length === 5) {
-            array.push(key);
-        }
-        if (obj[key].length === 5) {
-            array.push(obj[key]);
-        }
+  const result = [];
+  for (const key in obj) {
+    if (key.length === 5) {
+      result.push(key);
     }
-    return array;
+    if (typeof obj[key] === "string" && obj[key].length === 5) {
+      result.push(obj[key]);
+    }
+  }
+
+  return result;
+}
+
+/*  https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+Vasyliev M*/
+
+function buildFun(n) {
+  let res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      console.log(i);
+    });
+  }
+  return res;
+}
+
+/* 
+https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+Vasyliev M*/
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+
+  introduce() {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
 }
