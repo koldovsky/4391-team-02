3b35324bc0f3fefa5eb4ff7e34adc57173753489
Mishail Vasiluev Kn-222b <167981278+Mayschy@users.noreply.github.com>
2025-09-01 07:07:13 +0000

    Added new functionality to the intro, changed the styles of the header and special offers

--- js/booking-intro.js
+++ js/booking-intro.js
@@ -5,10 +5,19 @@ const openBookingIntroModalBtn = document.getElementById(
 const closeBookingIntroModalBtn = document.getElementById(
   "closeBookingIntroModalBtn"
 );
+const bookingForm = document.getElementById("bookingForm");
+const bookingIntroContent = document.getElementById("bookingIntroContent");
+const orderStatusContent = document.getElementById("orderStatusContent");
+const orderStatusText = document.getElementById("orderStatusText");
+const cancelOrderBtn = document.getElementById("cancelOrderBtn");
+const yourNameInput = document.getElementById("yourName");
+let orderIsActive = false;
 
 openBookingIntroModalBtn.addEventListener("click", () => {
-  bookingIntroModal.classList.add("show");
-  document.body.style.overflow = "hidden";
+  if (!orderIsActive) {
+    bookingIntroModal.classList.add("show");
+    document.body.style.overflow = "hidden";
+  }
 });
 
 closeBookingIntroModalBtn.addEventListener("click", () => {
@@ -22,3 +31,42 @@ window.addEventListener("click", (e) => {
     document.body.style.overflow = "auto";
   }
 });
+
+bookingForm.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+
+  const formIsValid = bookingForm.checkValidity();
+
+  if (formIsValid) {
+    bookingIntroModal.classList.remove("show");
+    document.body.style.overflow = "auto";
+
+    const userName = yourNameInput.value;
+    showOrderStatus(userName);
+
+    orderIsActive = true;
+
+    openBookingIntroModalBtn.disabled = true;
+  } else {
+    alert("Please fill out all required fields.");
+  }
+});
+
+cancelOrderBtn.addEventListener("click", () => {
+  resetOrderStatus();
+
+  openBookingIntroModalBtn.disabled = false;
+});
+
+function showOrderStatus(name) {
+  bookingIntroContent.style.display = "none";
+  orderStatusContent.style.display = "block";
+  orderStatusText.textContent = `Wait for your order, ${name}!`;
+}
+
+function resetOrderStatus() {
+  bookingIntroContent.style.display = "block";
+  orderStatusContent.style.display = "none";
+  bookingForm.reset(); 
+  orderIsActive = false;
+}
